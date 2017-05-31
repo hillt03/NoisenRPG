3ee13d95ecba2f435988f8b1f1e8505050a6c3a6
no1z <hillt03@yahoo.com>
2017-05-31 12:02:19 -0400

    Cleaned up using statements

--- NoisenRPG/Properties/Resources.Designer.cs
+++ NoisenRPG/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SuperAdventure.Properties {
+namespace NoisenRPG.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SuperAdventure.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SuperAdventure.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NoisenRPG.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- NoisenRPG/Properties/Settings.Designer.cs
+++ NoisenRPG/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SuperAdventure.Properties {
+namespace NoisenRPG.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
